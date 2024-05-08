9cc498cc74d4b4e33e462f1e4349a0dd5f9cc02a
Juanjo <jrjuanj01@penguin.com>
2024-05-08 15:06:34 -0400

    Added file 8 with permissions

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for l in str:
+        if ord('a') <= ord(l) <= ord('z'):
+            L = chr(ord(l) - 32)
+            print("{}".format(L), end="")
+    print()
