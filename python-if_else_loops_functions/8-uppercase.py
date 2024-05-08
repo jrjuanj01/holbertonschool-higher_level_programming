ebe4f561eec85a61e3bf311a53013372060b5f5a
Juanjo <jrjuanj01@penguin.com>
2024-05-08 15:08:39 -0400

    Added else condition to the function

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,6 @@ def uppercase(str):
         if ord('a') <= ord(l) <= ord('z'):
             L = chr(ord(l) - 32)
             print("{}".format(L), end="")
+        else:
+            print("{}".format(l), end="")
     print()
