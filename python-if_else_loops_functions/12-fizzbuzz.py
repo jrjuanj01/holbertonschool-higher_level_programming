40f999189fcb1805fd654559d045410009a5918e
Juanjo <jrjuanj01@penguin.com>
2024-05-08 16:00:48 -0400

    Added elif

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,9 @@ def fizzbuzz():
     for number in range(1, 101):
         if number % 15 == 0:
             print("FizzBuzz ", end="")
-        if number % 3 == 0:
+        elif number % 3 == 0:
             print("Fizz ", end="")
-        if number % 5 == 0:
+        elif number % 5 == 0:
             print("Buzz ", end="")
         else:
             print(f"{number} ", end="")
