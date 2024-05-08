717d3dce4e32a823ebe91ffc0dd9024052e9ad48
Juanjo <jrjuanj01@penguin.com>
2024-05-08 15:34:04 -0400

    Removed negative

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        n = -(-number % 10)
+        n = (-number % 10)
     else:
         n = number % 10
     print("{}".format(n), end="")
