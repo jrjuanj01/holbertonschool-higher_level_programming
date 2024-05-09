c5609212ac57ba3091b3da912f8173ac1267eb43
Juanjo <jrjuanj01@penguin.com>
2024-05-09 15:47:29 -0400

    Added file 2 with permissions

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+argc = len(sys.argv)
+idx = 0
+if argc == 1:
+    print("0 arguments.")
+elif argc == 2:
+    print("1 argument:")
+    print("1: {}".format(sys.argv[1]))
+elif argc > 2:
+    print("{} arguments:".format(argc - 1))
+    for args in sys.argv[1:]:
+        idx += 1
+        print("{}: {}".format((idx), args))
