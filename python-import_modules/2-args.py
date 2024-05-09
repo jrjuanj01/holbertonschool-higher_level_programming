95d0009437ee488013dc27225be9f66d0fcde6f6
Juanjo <jrjuanj01@penguin.com>
2024-05-09 16:18:04 -0400

    Added main condition to file 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,13 +2,14 @@
 import sys
 argc = len(sys.argv)
 idx = 0
-if argc == 1:
-    print("0 arguments.")
-elif argc == 2:
-    print("1 argument:")
-    print("1: {}".format(sys.argv[1]))
-elif argc > 2:
-    print("{} arguments:".format(argc - 1))
-    for args in sys.argv[1:]:
-        idx += 1
-        print("{}: {}".format((idx), args))
+if __name__ == "__main__":
+    if argc == 1:
+        print("0 arguments.")
+    elif argc == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    elif argc > 2:
+        print("{} arguments:".format(argc - 1))
+        for args in sys.argv[1:]:
+            idx += 1
+            print("{}: {}".format((idx), args))
