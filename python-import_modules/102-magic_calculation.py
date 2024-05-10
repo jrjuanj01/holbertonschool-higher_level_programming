a44ee07499948ef8619979016c0c2655c747a650
Juanjo <jrjuanj01@penguin.com>
2024-05-10 16:06:53 -0400

    Fixed Pycodestyle

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3ch
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
-    if __name__ == "__main__":
-        if a < b: 
-            c = add(a, b)
-            for i in range(4, 6):
-                c = c + i
-            return c
-        else:
-            return sub(a, b)
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
