dd480515df38c3496d2ab8dac51b6fc2a7e3fe70
Juanjo <jrjuanj01@penguin.com>
2024-05-13 15:03:16 -0400

    Fixed that one thing

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,8 @@
 #!/ustr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for nums in matrix:
-        idx = 0
-        for num in nums:
-            if idx < (len(nums)-1):
-                print("{:d}".format(num), end=" ")
-                idx += 1
-            else:
-                print("{:d}".format(num))
+        for idx in range(len(nums)):
+            print("{:d}".format(nums[idx]), end="")
+            if idx < len(nums) - 1:
+                print(" ", end="")
+        print()
