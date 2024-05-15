bf95240f01e37c17e6b1528db56920bf1b56a1ac
Juanjo <jrjuanj01@penguin.com>
2024-05-14 20:10:43 -0400

    Fixed pycode style

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,4 +3,4 @@ def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
     for key, values in new_dict.items():
         new_dict[key] = values * 2
-    return new_dict 
+    return new_dict
