b6292f12296021001e1320866fd2e64f07b93b16
Juanjo <jrjuanj01@penguin.com>
2024-05-15 13:41:31 -0400

    Added file 2 and 2-main

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    for idx in range(0, x):
+        try:
+            print("{:d}".format(my_list[idx]), end="")
+            i += 1
+        except (TypeError, ValueError):
+            pass
+    print()
+    return i
