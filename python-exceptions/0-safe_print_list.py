6d865548d6872450336f5fa34ab80d10c875e93d
Juanjo <jrjuanj01@penguin.com>
2024-05-15 12:10:35 -0400

    changed to .format style

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,11 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for idx in range(0, x):
-            print(f"{my_list[idx]}", end= "" if idx < x - 1 else "\n")
+            print("{}".format(my_list[idx]), end= "" if idx < x - 1 else "\n")
         digit = idx + 1
     except IndexError:
         for idx in range(0,):
-            print(f"{my_list[idx]}", end= "")
+            print("{}".format(my_list[idx]), end= "")
         print()
         digit = idx
     return digit
