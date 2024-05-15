67b53243b70acf1b29b3ed83b69025776dbbfde3
Juanjo <jrjuanj01@penguin.com>
2024-05-15 12:14:33 -0400

    Fixed Pycodestyle

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,11 +2,8 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for idx in range(0, x):
-            print("{}".format(my_list[idx]), end= "" if idx < x - 1 else "\n")
-        digit = idx + 1
+            print("{}".format(my_list[idx]), end="" if idx < x - 1 else "\n")
     except IndexError:
-        for idx in range(0,):
-            print("{}".format(my_list[idx]), end= "")
         print()
-        digit = idx
-    return digit
+        return idx
+    return idx + 1
