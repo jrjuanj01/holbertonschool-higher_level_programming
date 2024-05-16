c6687179563fed0e3421450596744562dff417db
Juanjo <jrjuanj01@penguin.com>
2024-05-16 14:23:44 -0400

    Fixed file 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,9 +5,8 @@
 class Square():
     """Documentation"""
     def __init__(self, size=0):
-        if size is not int:
-            raise TypeError
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError
-        else:
-            self.__size = size
+            raise ValueError("size must be >= 0")
+        self.__size = size
