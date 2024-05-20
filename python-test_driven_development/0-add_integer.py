3463b49b7d44a2fe770288d300d083121e122c21
Juanjo <jrjuanj01@penguin.com>
2024-05-20 14:05:37 -0400

    Vamo a ver

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,6 +8,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    if a is None or b is None:
-        raise TypeError("You must have two integers")
     return int(a) + int(b)
