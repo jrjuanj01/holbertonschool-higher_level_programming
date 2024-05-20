50d0388e4e0b6937cd33e1387f31ebe667fbc865
Juanjo <jrjuanj01@penguin.com>
2024-05-20 14:14:44 -0400

    Let's see

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,6 +6,9 @@ def add_integer(a, b=98):
     """
     Function that adds
     """
+    error = "unsupported operand type(s) for +: 'NoneType' and 'int'"
+    if a is None or b is None:
+        raise TypeError(error)
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
