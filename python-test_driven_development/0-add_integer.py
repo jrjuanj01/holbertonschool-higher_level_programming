60c1e18dfdd3e46045e6758aaefedaad0e7a5718
Juanjo <jrjuanj01@penguin.com>
2024-05-20 13:55:23 -0400

    Added none condition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that add two integers"""
+"""Script that adds two integers"""
 
 
 def add_integer(a, b=98):
@@ -8,4 +8,6 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
+    if a is None or b is None:
+        raise TypeError("You must have two integers")
     return int(a) + int(b)
