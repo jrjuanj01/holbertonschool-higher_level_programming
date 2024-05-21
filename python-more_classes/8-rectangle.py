c483694f895496d2dd7f68c4212df4d40d93a4b8
Juanjo <jrjuanj01@penguin.com>
2024-05-21 14:50:15 -0400

    Removed Whitespace

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -58,7 +58,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-        
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if isinstance(rect_1, Rectangle) is False:
