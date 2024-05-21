a74bb93f3fa02ee83c3bc4b83f03577436f9e8e7
Juanjo <jrjuanj01@penguin.com>
2024-05-21 12:53:13 -0400

    How about now

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,18 +10,18 @@ class Rectangle:
         if width < 0:
             raise ValueError("width must be >= 0")
         self.__width = width
-        
+
         if type(height) is not int:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
-        self.__height = height 
-        
+        self.__height = height
+
     @property
     def width(self):
         """"Returns width"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """Sets the value of width"""
@@ -35,8 +35,8 @@ class Rectangle:
     def heigth(self):
         """"Returns height"""
         return self.__height
-    
-    @height.setter
+
+    @heigth.setter
     def height(self, value):
         """Sets the value of height"""
         if type(value) is not int:
