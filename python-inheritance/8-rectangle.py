4ac69823e14ea55c4e289c37ac0460f27c4590a0
Juanjo <jrjuanj01@penguin.com>
2024-05-22 15:37:42 -0400

    Added file 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A class Rectangle that inherits from BaseGeometry"""
+
+
+BaseGeomerty = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeomerty):
+    """Class of Rectangle"""
+
+    def __init__(self, width, height):
+        """Initialization of a rectangle"""
+    
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
