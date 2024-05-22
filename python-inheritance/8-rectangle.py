1f4deaafc5424429f9a40b2dccef07f33ff3e296
Juanjo <jrjuanj01@penguin.com>
2024-05-22 15:52:32 -0400

    Try # 3

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -11,14 +11,8 @@ class Rectangle(BaseGeomerty):
     def __init__(self, width, height):
         """Initialization of a rectangle"""
 
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
+        super().integer_validator(width)
         self.__width = width
 
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
+        super().integer_validator(width)
         self.__height = height
