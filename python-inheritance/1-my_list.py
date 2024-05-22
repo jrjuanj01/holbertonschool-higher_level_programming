20a2ce0b9d29a942739d947dfc7e1f25c7f5da24
Juanjo <jrjuanj01@penguin.com>
2024-05-22 13:50:35 -0400

    : )

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,11 +5,6 @@
 class MyList(list):
     """New Class"""
 
-    def __init_subclass__(cls, value=None):
-        """Initializes Subclass list"""
-
-        cls.list = value
-
     def print_sorted(self):
         """A Method that prints the inherited attributes"""
 
