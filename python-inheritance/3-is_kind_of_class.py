406fa67160d3cc28654788f935fd0d96936b7fba
Juanjo <jrjuanj01@penguin.com>
2024-05-22 13:13:52 -0400

    Added fiel 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function to verify instance of type class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Veryfies if obj is instance of a_class"""
+
+    return isinstance(obj, a_class)
