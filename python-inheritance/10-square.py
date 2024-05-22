d7ff2321e6e5023cd5461a287829cec19f9757ea
Juanjo <jrjuanj01@penguin.com>
2024-05-22 16:28:56 -0400

    Added file 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -7,14 +7,14 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """A subclass of rectangle"""
-    
+
     def __init__(self, size):
         """Instancialization of a Square"""
-        
+
         super().integer_validator("size", size)
         self.__size = size
-    
+
     def area(self):
         """Returns the area of the square"""
-        
+
         return (self.__size ** 2)
