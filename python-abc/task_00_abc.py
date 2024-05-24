f5c33cc32976b2c7bac6b417595914589f17be06
Juanjo <jrjuanj01@penguin.com>
2024-05-24 10:15:35 -0400

    Added file )

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""Abstract classes"""
+
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    """Abstract class named Animal"""
+    
+    @abstractmethod
+    def sound(self):
+        pass
+    
+class Dog(Animal):
+    """Animal Dog"""
+    
+    def sound(self):
+        return "Bark"
+    
+class Cat(Animal):
+    """Animal Cat"""
+    
+    def sound(self):
+        return "Meow"
