c5bc490392efd32c97446f849d1db4e2c08afb07
Juanjo <jrjuanj01@penguin.com>
2024-05-24 14:38:51 -0400

    Added file 3 and 4

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""CountedIterator - Keeping Track of Iteration"""
+
+
+class CountedIterator:
+    """Class to mimic iter"""
+    def __init__(self, data):
+        self.data = iter(data)
+        self.counter = 0
+
+    def get_count(self):
+        """returns count"""
+        return self.counter
+
+    def __next__(self):
+        """override __next__"""
+        try:
+            item = next(self.data)
+            self.counter += 1
+            return item
+        except StopIteration:
+            raise StopIteration
--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""The Enigmatic FlyingFish - Exploring Multiple Inheritance"""
+
+
+class Fish:
+    """fish class"""
+    def swim(self):
+        print("The fish is swimming")
+
+    def habitat(self):
+        print("The fish lives in water")
+
+
+class Bird:
+    """bird class"""
+    def fly(self):
+        print("the bird is flying")
+
+    def habitat(self):
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """FlyingFish class"""
+    def fly(self):
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
