62a998229bac7c55d7ffae22a43559d11eb8ccc0
Juanjo <jrjuanj01@penguin.com>
2024-05-24 14:31:55 -0400

    Added file 5

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""The Mystical Dragon - Mastering Mixins"""
+
+
+class SwimMixin():
+    """Swim Class"""
+    
+    def swim(self):
+        print("The creature swims!")
+
+class FlyMixin():
+    """Fly Class"""
+    
+    def fly(self):
+        print("The creature flies!")
+    
+class Dragon(SwimMixin, FlyMixin):
+    """Dragon that inherits from both SwimMixin and FlyMixin"""
+    
+    def roar(self):
+        print("The dragon roars!")
