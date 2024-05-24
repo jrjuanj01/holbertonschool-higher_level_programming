2eb3dc3a43e0f709db95be15a4810a881383c977
Juanjo <jrjuanj01@penguin.com>
2024-05-24 11:14:44 -0400

    Added file 2

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,1 +1,50 @@
 #!/usr/bin/python3
+"""Duck Typing"""
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+    """Shape class"""
+    
+    @abstractmethod
+    def area(self):
+        pass
+    
+    @abstractmethod
+    def perimeter(self):
+        pass
+    
+class Circle(Shape):
+    """Circle class That inherits from Shape"""
+    
+    def __init__(self, radius):
+        if type(radius) in [int, float]:
+            self.radius = radius
+    
+    def area(self):
+        return (pi * (self.radius ** 2))
+    
+    def perimeter(self):
+        return (2 * pi * self.radius)
+    
+class Rectangle(Shape):
+    """Rectangle class"""
+    def __init__(self, width=0, height=0):
+        if type(width) in [int, float]:
+            self.width = width
+
+        if type(height) in [int, float]:
+            self.height = height
+        
+    def area(self):
+        return (self.width * self.height)
+    
+    def perimeter(self):
+        return (2 * self.height) + (2 * self.width)
+
+def shape_info(shape):
+    """Prints the information of a shape"""
+    
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
