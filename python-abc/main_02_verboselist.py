8a55acd185220c89e98c58f6a9c6cca06034d7b4
Juanjo <jrjuanj01@penguin.com>
2024-05-24 14:07:39 -0400

    Added task 2

--- python-abc/main_02_verboselist.py
+++ python-abc/main_02_verboselist.py
@@ -4,6 +4,6 @@ from task_02_verboselist import VerboseList
 vl = VerboseList([1, 2, 3])
 vl.append(4)
 vl.extend([5, 6])
-vl.remove(2)
+vl.remove(2)S
 vl.pop()
 vl.pop(0)
--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 """Extending the Python List with Notifications"""
-__import__(list)
+__import__("sys")
 
 
 class VerboseList(list):
     """List modifying Class"""
     
     
+    def append(self, object) :
+        print(f"Added [{object}] to the list.")
+        return super().append(object)
+
+    def extend(self, iterable):
+        print(f"Extended the list with [{len(iterable)}] items.")
+        return super().extend(iterable)
     
+    def remove(self, value):
+        print(f"Removed [{value}] from the list.")
+        return super().remove(value)
+    
+    def pop(self, index=-1):
+        print(f"Popped [{self[index]}] from the list.")
+        return super().pop(index)
