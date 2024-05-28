2e8eaa1919e8918f90edb731ee52ded58e618147
Juanjo <jrjuanj01@penguin.com>
2024-05-28 10:40:05 -0400

    Test 1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Opens and reads file"""
+
+
 def read_file(filename=""):
+    """Opens, reads, and prints the file given"""
+
     with open(filename, "r") as file:
         content = file.read()
         print(content)
