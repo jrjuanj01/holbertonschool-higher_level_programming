a90525f2cc33cee9edbd88d0061e16511be00262
Juanjo <jrjuanj01@penguin.com>
2024-05-28 10:44:17 -0400

    Test #2

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
     """Opens, reads, and prints the file given"""
 
-    with open(filename, "r") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         content = file.read()
-        print(content)
+        print(content, end="")
