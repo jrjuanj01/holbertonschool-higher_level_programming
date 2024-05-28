86a3704ff323769c57239cc4f814dd18cd86452e
Juanjo <jrjuanj01@penguin.com>
2024-05-28 15:09:23 -0400

    now

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,9 +4,8 @@
 
 from sys import argv
 
-if __name__ == "__main__":
-    save_to_json = __import__("5-save_to_json_file").save_to_json_file
-    load_from_json = __import__("6-load_from_json_file").load_from_json_file
+save_to_json = __import__("5-save_to_json_file").save_to_json_file
+load_from_json = __import__("6-load_from_json_file").load_from_json_file
   
 if len(argv) > 0:
     my_list = []
