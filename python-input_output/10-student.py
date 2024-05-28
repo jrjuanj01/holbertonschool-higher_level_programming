bd162f946aef2c3d9b77e937baba6464aab7a2c3
Juanjo <jrjuanj01@penguin.com>
2024-05-28 16:57:48 -0400

    Added file 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Student Class Module"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instance definition"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        """Converts instance to JSON format"""
+        result = {}
+        for key, value in self.__dict__.items():
+            if key in attr:
+                result[key] = value
+        return result
