2f797bce738f5bfbfb947cc841bbac6e05b0ee4e
Juanjo <jrjuanj01@penguin.com>
2024-05-28 15:06:41 -0400

    Added file 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A script that adds all arguments to a file"""
+from sys import argv
+
+if __name__ == "__main__":
+    save_to_json = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json = __import__("6-load_from_json_file").load_from_json_file
+
+  
+if len(argv) > 0:
+    my_list = []
+    for args in argv:
+        my_list.append(args)
+    my_list.pop(0)
+try:
+    old_list = load_from_json("add_item.json")
+except FileNotFoundError:
+    old_list = []
+new_list = old_list + my_list
+save_to_json(new_list, "add_item.json")
