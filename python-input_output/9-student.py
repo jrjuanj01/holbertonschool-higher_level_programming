59c75bba95572d7d8283f26b376291f041112ace
Juanjo <jrjuanj01@penguin.com>
2024-05-28 16:53:08 -0400

    Fixed pycodestyle

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,14 +4,14 @@
 
 class Student:
     """Student class"""
-    
+
     def __init__(self, first_name, last_name, age):
         """Instance definition"""
-        
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self):
         """Converts instance to JSON format"""
         result = {}
