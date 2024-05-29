91d66a6d2eba327c39566110d48cd3d40198a259
Juanjo <jrjuanj01@penguin.com>
2024-05-29 10:25:16 -0400

    Added task_00

---
+++ python -serialization/task_00_basic_serialization.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Basic serialization module from Python to JSON"""
+
+
+import json
+
+def serialize_and_save_to_file(data, filename):
+    """Serializes and saves data to a file"""
+
+    with open(filename, "w", encoding="utf-8") as jfile:
+        json.dump(data, jfile)
+
+def load_and_deserialize(filename):
+    """Loads and deserializes data from a file"""
+
+    with open(filename, "r", encoding="utf-8") as jfile:
+        return json.load(jfile)
