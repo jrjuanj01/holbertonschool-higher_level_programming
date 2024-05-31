4c58b0fc24fd87e316a92c8161d06d89a7850d81
Juanjo <jrjuanj01@penguin.com>
2024-05-31 17:51:19 -0400

    i

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,30 +1,38 @@
 """Developing a Simple API using Python with Flask"""
 from flask import Flask, jsonify, request
 
-
 app = Flask(__name__)
 
-
+# user data
 users = {}
 
 
-@app.route('/')
+@app.route("/")  # Route for the root URL
 def home():
-    return 'Welcome to the Flask API!'
+    return "Welcome to the Flask API!"
 
 
-@app.route('/data')
+@app.route("/data")  # Route to get list of usernames
 def data():
     return jsonify(list(users.keys()))
 
 
-@app.route("/status")
+@app.route("/status")  # Route to check status
 def status():
-    return 'OK'
+    return "OK"
 
 
-@app.route('/add_user', methods=['POST'])
-def add_user():
+@app.route("/users/<username>")  # Route to get user details by username
+def find(username):
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+
+@app.route('/add_user', methods=['POST'])  # Route to add a new user
+def add():
     newuser = request.json
     username = newuser.get("username")
 
@@ -36,13 +44,5 @@ def add_user():
     return jsonify({"message": "User added", "user": newuser}), 201
 
 
-@app.route("/user/<username>")
-def profile(username):
-    if username not in users:
-        return jsonify({'error': 'User does not exist'}), 400
-    else:
-        return users[username]
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)  # Run the Flask development server
