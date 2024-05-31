c332d26b7fea87dd35503b4ac8bbebaccc615aa0
Juanjo <jrjuanj01@penguin.com>
2024-05-31 13:45:12 -0400

    now?

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -36,7 +36,7 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(b"OK")
 
         else:
-            self.send_response(404 Not Found)
+            self.send_response(404)
             self.send_header("Content-type", "text/html")
             self.end_headers()
             self.wfile.write(b"Endpoint Not Found")
