5c20b14cacfc9ec37062441b8e13948512386213
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-17 17:51:52 -0400

    Test #5

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,11 @@
 -- Block
-CREATE TABLE INTO hbtn_0c_0 IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT)
+CREATE TABLE IF NOT EXISTS second_table (id INT PRIMARY KEY, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score)
 VALUES (1, 'John', 10),
     (2, 'Alex', 3),
     (3, 'Bob', 14),
-    (4, "George", 8);
+    (4, "George", 8) ON DUPLICATE KEY
+UPDATE name =
+VALUES(name),
+    score =
+VALUES(score);
