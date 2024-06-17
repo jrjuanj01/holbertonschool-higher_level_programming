79d042d38ada361a3a01cf83631144534a2447ab
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-17 17:48:48 -0400

    Test #3

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Block
-DESCRIBE CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT)
+CREATE TABLE INTO hbtn_0c_0 IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT)
 INSERT INTO second_table (id, name, score)
 VALUES (1, 'John', 10),
     (2, 'Alex', 3),
