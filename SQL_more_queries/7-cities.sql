c8e8734f1f59adef19fb92af1d3b4e3600b7de8a
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-18 15:24:36 -0400

    Test #5

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     states_id INT NOT NULL,
-    FOREIGN KEY (states_id) REFERENCES states (id),
+    FOREIGN KEY (states_id) REFERENCES hbtn_0d_usa.states (id),
     name VARCHAR(256) NOT NULL
 );
