73018553880f03aa7538a68f15b2d23cd575c624
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-18 15:17:44 -0400

    Added task #7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- Block
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    FOREIGN KEY (states_id NOT NULL) REFERENCES states (id),
+    name VARCHAR(256) NOT NULL
+);
