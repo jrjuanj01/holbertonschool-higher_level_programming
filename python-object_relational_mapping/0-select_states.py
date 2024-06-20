c6333d40ed92cb9b1dcfb6b6794126f2ca4e2ac2
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-20 13:32:49 -0400

    Added task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,2 +1,26 @@
+#!/usr/bin/python3
 import MySQLdb
 
+def connect_to_mysql(username, password, database_name):
+    """Connects to MySQL server"""
+    
+    try:
+        conn = MySQLdb.connect(host="localhost",
+                            port="33060",
+                            user=username,
+                            password=password,
+                            db=database_name)
+        cursor = conn.cursor()
+        
+        cursor.execute("SELECT * FROM staes ORDER BY states.id ASC")
+        
+        states = cursor.fetchall()
+        
+        for state in states:
+            print(state)
+            
+        cursor.close()
+        conn.close
+        
+    except MySQLdb.Error as e:
+        print(f"Error connectig to MySQL or execuiting query: {e}")
