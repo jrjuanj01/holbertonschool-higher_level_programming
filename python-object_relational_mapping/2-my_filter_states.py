9f821a29807b8a4987744dd79153c1826d84d457
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-20 16:02:47 -0400

    Test #1

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,7 @@ def list_states(username, password, database_name, search):
                                password=password,
                                db=database_name)
         cursor = conn.cursor()
-        cursor.execute(f"SELECT * FROM states WHERE (name) LIKE BINARY\
+        cursor.execute(f"SELECT * FROM states WHERE (name) = \
                         '{search}' ORDER BY states.id ASC;")
         states = cursor.fetchall()
         for state in states:
