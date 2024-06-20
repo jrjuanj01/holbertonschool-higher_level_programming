7c79745fc7c8d28bd1c3a24836829fd2b50c5c25
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-20 14:46:03 -0400

    Added task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Lists all states in a database"""
+import MySQLdb
+import sys
+
+
+def list_states(username, password, database_name):
+    """Connects to MySQL server"""
+
+    try:
+        conn = MySQLdb.connect(host='localhost',
+                               port=3306,
+                               user=username,
+                               password=password,
+                               db=database_name)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM states"
+                       "WHERE name LIKE 'N%' ORDER BY states.id ASC")
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
+
+        cursor.close()
+        conn.close()
+
+    except MySQLdb.Error as e:
+        print(f"Error connectig to MySQL or execuiting query: {e}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python3 0-selsct_states.py username password database")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database_name = sys.argv[3]
+        list_states(username, password, database_name)
