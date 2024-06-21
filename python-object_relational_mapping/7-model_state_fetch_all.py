407a1a4cc1ee676787d8d6b5251440d2f373b190
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-21 14:13:42 -0400

    Test #5

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,7 +2,7 @@
 """Fetches states from dtatbase"""
 from model_state import Base, State
 import sys
-from sqlalchemy import create_engine, sessionmaker
+from sqlalchemy import create_engine, session_maker
 
 
 if __name__ == "__main__":
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    Session = session_maker(bind=engine)
     session = Session
     states = session.query(State).order_by(State.id).all()
     for state in states:
