0de1eba22aa78716911ec239b36b81d22314d46f
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-21 13:42:07 -0400

    Added task #7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Wrong usage")
+
+    else:
+        State(sys.argv[1], sys.argv[2], sys.argv[3])
