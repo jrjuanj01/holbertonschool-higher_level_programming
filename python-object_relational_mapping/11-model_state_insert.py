7b78e8f314817ea5ec90b78ef2b3ca5e42b7994b
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-21 15:26:14 -0400

    Test #1

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    s1 = State("Louisiana")
+    s1 = State(name="Louisiana")
     session.add(s1)
     session.commit()
     session.close()
