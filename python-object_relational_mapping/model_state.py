8524b20322d4020e1c0829c39da0f15d131f7362
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-21 13:04:01 -0400

    Added task #6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""First state model"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class State(Base):
+    """Base class for the states table"""
+    __tablename__ = "states"
+    
+    
+    id = Column("id", Integer, primary_key=True)
+    name = Column("name", String(128), nullable=False)
+    
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name 
