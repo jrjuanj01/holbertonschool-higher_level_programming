f43233f69e389451af73e16386b014ba70604bbd
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-21 15:43:15 -0400

    Added task #13

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Fetches states from dtatbase"""
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """create engine"""
+    if len(sys.argv) != 4:
+        print("wrong")
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).where(State.name.
+                                        like('%a%')).order_by(State.id).all()
+    for state in states:
+        session.delete(state)
+    session.close()
