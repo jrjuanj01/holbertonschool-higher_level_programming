0de9c22405ae6f202a6d3b99b854acc72e4b875d
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-21 13:19:26 -0400

    Test #2

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -11,12 +11,5 @@ class State(Base):
     """Base class for the states table"""
     __tablename__ = "states"
 
-    id = Column("id", Integer, primary_key=True)
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
     name = Column("name", String(128), nullable=False)
-
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
-    def __repr__(self):
-        return f"{self.id} {self.name}"
