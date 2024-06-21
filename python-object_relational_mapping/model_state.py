3b38f2fa13b75f6311c60b18a6cb944ba5b5689f
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-21 13:14:29 -0400

    Test #1

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,14 +6,17 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """Base class for the states table"""
     __tablename__ = "states"
-    
-    
+
     id = Column("id", Integer, primary_key=True)
     name = Column("name", String(128), nullable=False)
-    
+
     def __init__(self, id, name):
         self.id = id
-        self.name = name 
+        self.name = name
+
+    def __repr__(self):
+        return f"{self.id} {self.name}"
