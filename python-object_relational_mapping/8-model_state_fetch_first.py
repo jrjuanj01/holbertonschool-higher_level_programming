89418984badf5d800c19f09230967cd491e5feb8
jrjuanj01 <jrjuanj01@gmail.com>
2024-06-21 14:37:48 -0400

    Added task #8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Fetches states from dtatbase"""
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """create engine"""
+    if len(sys.argv) != 4:
+        print("wrong")
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+    state = states[0]
+    print(f"{state.id}: {state.name}")
+    session.close()
