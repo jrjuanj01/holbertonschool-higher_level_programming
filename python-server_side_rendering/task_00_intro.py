dbe5337fc7e0fd644366c41034ab2b5b38f38a93
jrjuanj01 <jrjuanj01@gmail.com>
2024-07-15 21:04:51 -0400

    Added task 0

---
+++ python-server_side_rendering/task_00_intro.py
@@ -0,0 +1,20 @@
+"""Function to Generate invitations"""
+import os
+
+def generate_invitations(template:str, attendees:list):
+    """Create an invitstion based on the template an filling in the information with the list"""
+    
+    try:
+        if type(template) is not str:
+            raise TypeError("Template must be a string")
+        if type(attendees) is not list:
+            raise TypeError("Attendees must be a list")
+        for attendee in attendees:
+            if type(attendee) is not dict:
+                raise TypeError("Attendees must be a list of dictionaries")
+            fixed_attendee = {key: (value if value is not None else "N/A") for key, value in attendee.items()}
+            x = attendees.index(attendee) + 1
+            with open(f"output_{x}.txt", "w") as file:
+                file.write(template.format(**fixed_attendee))
+    except TypeError as e:
+        print(f"Error: {str(e)}")
