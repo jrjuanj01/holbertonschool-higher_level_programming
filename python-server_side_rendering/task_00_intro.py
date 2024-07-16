b4b22a28635063117f52f08732a7ff7881d22602
jrjuanj01 <jrjuanj01@gmail.com>
2024-07-15 22:17:49 -0400

    Added epmty template error

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -4,23 +4,23 @@ import os
 def generate_invitations(template:str, attendees:list):
     """Create an invitstion based on the template an filling in the information with the list"""
     
-    try:
-        if type(template) is not str:
-            raise TypeError("Template must be a string")
-        if type(attendees) is not list:
-            raise TypeError("Attendees must be a list")
-        if not template.strip():
-            print("Error: Template is empty, no output files generated.")
-            return
-        if not attendees:
-            print("Error: No data provided, no output files generated.")
-            return
-        for attendee in attendees:
-            if type(attendee) is not dict:
-                raise TypeError("Attendees must be a list of dictionaries")
-            fixed_attendee = {key: (value if value is not None else "N/A") for key, value in attendee.items()}
-            x = attendees.index(attendee) + 1
+    if type(template) is not str:
+        raise TypeError("Template must be a string")
+    if type(attendees) is not list:
+        raise TypeError("Attendees must be a list")
+    if not template.strip():
+        print("Error: Template is empty, no output files generated.")
+        return
+    if not attendees:
+        print("Error: No data provided, no output files generated.")
+        return
+    for attendee in attendees:
+        if type(attendee) is not dict:
+            raise TypeError("Attendees must be a list of dictionaries")
+        fixed_attendee = {key: (value if value is not None else "N/A") for key, value in attendee.items()}
+        x = attendees.index(attendee) + 1
+        try:
             with open(f"output_{x}.txt", "w") as file:
-                file.write(template.format(**fixed_attendee))
-    except TypeError as e:
-        print(f"Error: {str(e)}")
+                file.write(template.format(**fixed_attendee))    
+        except TypeError as e:
+            print(f"Error: {str(e)}")
