0056d51f09c83470593ff83c6cce4d8122a01732
jrjuanj01 <jrjuanj01@gmail.com>
2024-07-28 23:34:17 -0400

    Added file 7

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,16 +1,20 @@
-function fetchCharFromAPI() {
-    const api = "https://swapi-api.hbtn.io/api/people/5/?format=json";
-    const character = document.querySelector("#character");
-
-    fetch(api)
-        .then((response) => response.json())
-        .then((data) => {
-            const charName = data.name;
-            character.textContent = charName;
+addEventListener("DOMContentLoaded", function () {
+    // Using promises because of fetch...
+    fetch("https://swapi-api.hbtn.io/api/films/?format=json")
+        .then((response) => {
+            // If status ok return content from fetch for next .then
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
         })
-        .catch((error) => {
-            console.error("Error: ", error);
+        .then((content) => {
+            // After awaiting results make new <li> with the movie title as text
+            const movies = content.results;
+            for (movie of movies) {
+                const li = document.createElement("li");
+                li.innerText = movie.title;
+                document.querySelector("#list_movies").append(li);
+            }
         });
-}
-
-fetchCharFromAPI();
+});
